1f45715cf56947b433d0fe576ba3662135c1b0b3
Rodrigo Sasaki <rodrigopsasaki@gmail.com>
2016-04-30 20:13:18 -0400

    New class to store error messages

---
+++ src/main/java/com/rodrigosasaki/taxi/utils/ErrorMessage.java
@@ -0,0 +1,9 @@
+package com.rodrigosasaki.taxi.utils;
+
+public class ErrorMessage {
+
+    public static final String UNWALKABLE_COORDINATE = "The specified coordinate (%d, %d) is not walkable in the grid, please choose a different coordinate";
+    public static final String INEXISTENT_COORDINATE = "The specified coordinate (%d, %d) does not exist in the grid, please choose a different coordinate";
+    public static final String UNREACHABLE_DESTINATION = "The passenger's destination is unreachable from his source position";
+
+}
